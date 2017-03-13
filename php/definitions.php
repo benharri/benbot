d0850e22f17af9ad6bbe88d0d6d51638c6825bfe
benharri <benharri@nmu.edu>
2017-03-13 16:46:41 -0400

    definitions are now an array internally instead of a generic object

--- php/definitions.php
+++ php/definitions.php
@@ -1,37 +1,37 @@
 <?php
 
 class Definitions {
-    protected $definitions;
+    protected $defs;
     protected $filepath;
 
     public function __construct($filepath = __DIR__.'/definitions.json') {
         $this->filepath = $filepath;
-        $this->definitions = json_decode(file_get_contents($filepath));
+        $this->defs = json_decode(file_get_contents($filepath), true);
     }
 
     public function __toString() {
-        return print_r($this->definitions, true);
+        return print_r($this->defs, true);
     }
 
     public function get($key) {
-        return $this->definitions->$key ?? "**not set**";
+        return $this->defs[$key] ?? "**not set**";
     }
 
     public function set($key, $val) {
-        $this->definitions->$key = $val;
+        $this->defs[$key] = $val;
         $this->save();
     }
 
     public function unset($key) {
-        unset($this->definitions->$key);
+        unset($this->defs[$key]);
         $this->save();
     }
 
     public function list_keys() {
-        return array_keys((array)$this->definitions);
+        return array_keys($this->defs);
     }
 
     public function save() {
-        file_put_contents($this->filepath, json_encode($this->definitions));
+        file_put_contents($this->filepath, json_encode($this->defs));
     }
 }
