515381730a5cf0c4751cc6db82bab047a86cc327
Ben Harris <mail@benharris.ch>
2017-03-20 10:46:14 -0400

    fricken embeds

--- bot.php
+++ bot.php
@@ -814,29 +814,27 @@ $discord->registerCommand('server', function($msg, $args) use ($discord) {
         ]),
         'description' => "server info:\n",
         'fields' => $discord->factory(Collection::class, [
-            'items' => [
-                $discord->factory(Field::class, [
-                    'name' => 'Member Count',
-                    'value' => $guild->member_count,
-                    'inline' => true,
-                ]),
-                // $discord->factory(Field::class, [
-                //     'name' => 'Region',
-                //     'value' => $guild->region,
-                // ]),
-                // $discord->factory(Field::class, [
-                //     'name' => 'Owner',
-                //     'value' => $guild->owner,
-                // ]),
-                // $discord->factory(Field::class, [
-                //     'name' => 'Is large?',
-                //     'value' => $guild->large,
-                // ]),
-                // $discord->factory(Field::class, [
-                //     'name' => 'Verification level',
-                //     'value' => $guild->verification_level,
-                // ]),
-            ],
+            $discord->factory(Field::class, [
+                'name' => 'Member Count',
+                'value' => $guild->member_count,
+                'inline' => true,
+            ]),
+            // $discord->factory(Field::class, [
+            //     'name' => 'Region',
+            //     'value' => $guild->region,
+            // ]),
+            // $discord->factory(Field::class, [
+            //     'name' => 'Owner',
+            //     'value' => $guild->owner,
+            // ]),
+            // $discord->factory(Field::class, [
+            //     'name' => 'Is large?',
+            //     'value' => $guild->large,
+            // ]),
+            // $discord->factory(Field::class, [
+            //     'name' => 'Verification level',
+            //     'value' => $guild->verification_level,
+            // ]),
         ]),
     ]);
     send($msg, "", $embed);
